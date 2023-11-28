6fa1d132654214549c0a7873dcd07663c4b95e4d
JustAQuickName <51916221+JustAQuickName@users.noreply.github.com>
2023-11-28 10:06:39 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Nicks Repo',
+    description: 'My personal store of Kasm-supported Docker Containers.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/JustAQuickName/Custom-Kasm/',
+    contactUrl: 'https://github.com/JustAQuickName/Custom-Kasm',
   },
   reactStrictMode: true,
   swcMinify: true,
