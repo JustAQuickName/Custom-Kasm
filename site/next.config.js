667b40c8b98d88e3752ea231c81cc2a2ebe981e9
JustAQuickName <51916221+JustAQuickName@users.noreply.github.com>
2023-11-29 07:54:39 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/1.0',
+  basePath: '/Custom-Kasm/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
